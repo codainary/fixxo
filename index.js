4093f0d69a19df61f394c740b2b4da50bba0da2b
Hernan Pereira <pereirahernan@outlook.com>
2022-11-09 14:56:22 -0500

    edit existing port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler, uploadErrorH
 const { checkApiKey } = require('./middlewares/auth.handler');
 
 const app = express();
-const port = process.env.PORT || 2022;
+const port = process.env.PORT || 3000;
 
 //app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
